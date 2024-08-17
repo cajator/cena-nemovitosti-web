cc748be0c3afc71763e85fc123784d74f94d4fd8
cajator <caja@centrum.cz>
2024-08-17 14:18:23 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('estimate-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Děkujeme za váš zájem. Budeme vás kontaktovat do 24 hodin s odhadem ceny vaší nemovitosti.');
+    // Zde byste normálně odeslali data na server
+});
